86d09d756975ff8f7277344361ddaba37295bc61
esmarr58 <ruben.estrada@hetpro.com.mx>
2025-02-06 06:33:47 -0600

    Update pwm-intermedio-input.c

--- pwm-intermedio-input.c
+++ pwm-intermedio-input.c
@@ -3,13 +3,16 @@
 #include "driver/gpio.h"
 #include "esp_timer.h"
 #include <stdio.h>
+#include "esp_task_wdt.h"
 
-#define PIN_LED 12
+#define PIN_LED 40
 #define PIN_INPUT 47  // Pin de entrada para modificar el ciclo de trabajo
 #define ALTO 1
 #define BAJO 0
 
 void app_main(void) {
+    esp_task_wdt_deinit();
+    
     gpio_reset_pin(PIN_LED);
     gpio_set_direction(PIN_LED, GPIO_MODE_OUTPUT);
 
@@ -18,7 +21,7 @@ void app_main(void) {
     gpio_set_pull_mode(PIN_INPUT, GPIO_PULLDOWN_ONLY); // Evita fluctuaciones
 
     uint32_t frecuencia = 50;      // Frecuencia inicial en Hz
-    uint32_t dutyCycle = 10;       // Ciclo de trabajo inicial en porcentaje
+    uint8_t dutyCycle = 10;       // Ciclo de trabajo inicial en porcentaje
     uint32_t intervaloCambio_ms = 500; // Tiempo en ms antes de cambiar el PWM
 
     int64_t tiempoCambio = esp_timer_get_time(); // Marca de tiempo inicial
@@ -60,7 +63,7 @@ void app_main(void) {
                 if (dutyCycle > 100) {
                     dutyCycle = 0; // Reinicia el ciclo de trabajo
                 }
-                printf("Botón presionado. Nuevo ciclo de trabajo: %d%%\n", dutyCycle);
+                printf("Botón presionado. Nuevo ciclo de trabajo: %d\n", dutyCycle);
             }
         }
     }
